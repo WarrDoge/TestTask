57b830d8a01572feab169e2867f20a1378912518
WarrDoge <warr.doge@gmail.com>
2021-04-20 12:09:11 +0300

    1.06

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env/ bash 
 if [[ "$1" == "ansible" ]]; then
 	docker build -f ansible_container . -t ansible && docker run ansible 
-elif [[ "$1" == "ansible -it" ]]; then
+elif [[ "$1" == "ansible-it" ]]; then
 	docker build -f ansible_container . -t ansible && docker run -it --entrypoint /bin/bash ansible
 elif [[ "$1" == "terraform" ]]; then
 	docker build -f terraform_container . -t terraform && docker run terraform
-elif [[ "$1" == "terraform -it" ]]; then
+elif [[ "$1" == "terraform-it" ]]; then
 	docker build -f terraform_container . -t terraform && docker run -it --entrypoint /bin/bash terraform
 else
 	echo "Unknown container"
