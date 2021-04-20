f1daee8507319ab7890ba09ff9c04d6450195a5e
WarrDoge <warr.doge@gmail.com>
2021-04-20 09:40:35 +0300

    1.01

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env/ bash 
 if [[ "$1" == "ansible" ]]; then
-	docker build ansible_container
+	docker build -f ansible_container . -t ansible && docker run ansible 
+	# if interective mode is needed: && docker run -it --entrypoint /bin/bash ansible
 elif [[ "$1" == "terraform" ]]; then
-	docker build terraform_container
+	docker build -f terraform_container . -t terraform && docker run terraform
+	# if interective mode is needed: && docker run -it --entrypoint /bin/bash terraform
 else
 	echo "Unknown container"
 fi
