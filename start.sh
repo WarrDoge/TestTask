ed73ea49df4756cf00ea8b40e435e6d4b5d447a9
WarrDoge <warr.doge@gmail.com>
2021-04-20 11:02:08 +0300

    Added -it mode

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env/ bash 
 if [[ "$1" == "ansible" ]]; then
 	docker build -f ansible_container . -t ansible && docker run ansible 
-	# if interective mode is needed: && docker run -it --entrypoint /bin/bash ansible
+elif [[ "$1" == "ansible -it" ]]; then
+	docker build -f ansible_container . -t ansible && docker run -it --entrypoint /bin/bash ansible
 elif [[ "$1" == "terraform" ]]; then
 	docker build -f terraform_container . -t terraform && docker run terraform
-	# if interective mode is needed: && docker run -it --entrypoint /bin/bash terraform
+elif [[ "$1" == "terraform -it" ]]; then
+	docker build -f terraform_container . -t terraform && docker run -it --entrypoint /bin/bash terraform
 else
 	echo "Unknown container"
 fi
