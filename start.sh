4aaa1980637246e28a035c4ca90092fc5dd05948
WarrDoge <warr.doge@gmail.com>
2021-04-21 14:12:21 +0300

    Added docker-compose and nginx conf

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env/ bash 
 if [[ "$1" == "ansible" ]]; then
-	docker build -f ansible_container . -t ansible && docker run ansible 
+	docker build -f ansible_container . -t ansible && docker run  -v ./ansible ansible
 elif [[ "$1" == "ansible-it" ]]; then
-	docker build -f ansible_container . -t ansible && docker run -it --entrypoint /bin/bash ansible
+	docker build -f ansible_container . -t ansible && docker run -v ./ansible -it --entrypoint /bin/bash ansible
 elif [[ "$1" == "terraform" ]]; then
-	docker build -f terraform_container . -t terraform && docker run terraform
+	docker build -f terraform_container . -t terraform && docker run -v ./terraform terraform
 elif [[ "$1" == "terraform-it" ]]; then
-	docker build -f terraform_container . -t terraform && docker run -it --entrypoint /bin/bash terraform
+	docker build -f terraform_container . -t terraform && docker run -v ./terraform -it --entrypoint /bin/bash terraform
 else
 	echo "Unknown container"
 fi
